112be0682fd12671a63fd63ff69c6377a617480d
Srikanth Astaputhra <sastaputhra@enova.com>
2015-07-13 21:31:43 -0500

    formatted go code

--- large_sum.go
+++ large_sum.go
@@ -1,34 +1,32 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-    "strings"
-    "math/big"
+	"fmt"
+	"io/ioutil"
+	"math/big"
+	"strings"
 )
 
-
-
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
 
 func main() {
-  dat, err := ioutil.ReadFile("/tmp/numbers")
-  check(err)
-  contents := strings.Split(string(dat),"\n")
-  sum := big.NewInt(0)
-  fmt.Println(sum)
-  bi := big.NewInt(0)
-  for i := 0; i < 100; i++ {
-    if _, ok := bi.SetString(contents[i], 10); ok {
-      //fmt.Printf("%v\n", bi)
-      sum.Add(sum, bi)
-    } else {
-      fmt.Printf("error parsing line %#v\n", i)
-    }
-  }
-  fmt.Println(sum.String()[0:10])
+	dat, err := ioutil.ReadFile("/tmp/numbers")
+	check(err)
+	contents := strings.Split(string(dat), "\n")
+	sum := big.NewInt(0)
+	fmt.Println(sum)
+	bi := big.NewInt(0)
+	for i := 0; i < 100; i++ {
+		if _, ok := bi.SetString(contents[i], 10); ok {
+			//fmt.Printf("%v\n", bi)
+			sum.Add(sum, bi)
+		} else {
+			fmt.Printf("error parsing line %#v\n", i)
+		}
+	}
+	fmt.Println(sum.String()[0:10])
 }
